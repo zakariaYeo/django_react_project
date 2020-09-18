ea70d4d801e647d984c967072f723af1a1318d4e
zakariayeo <yeominfongazakaria@gmail.com>
2020-09-18 15:05:05 +0000

    initial commit

---
+++ students_react/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="text-center">
+                <hr />
+                <h1>App with React + Django</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
